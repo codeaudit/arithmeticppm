c955d9101c291fbde48688754a392efa4affbff8
dienertalencar <dienertalencar@0f736a00-6b68-11de-9c53-9da96a4633ec>
2009-07-15 02:50:42 +0000

    teste

---
+++ trunk/ppmccod/funcoesppmc.c
@@ -0,0 +1,16 @@
+/****
+ *
+ * Arquivo: funcoesppmc.c
+ *
+ * Autor: Gutenberg Pessoa Botelho Neto
+ *
+ * Data de Criação: 13/07/2009
+ * Última modificação: 13/07/2009
+ *
+ * Descrição: Implementacao das funcoes utilizadas para o programa de PPM-C
+ *
+ ****/
+
+#include "funcoesppmc.h"
+
+// ...
